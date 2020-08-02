f1f5a29f6d5d4cacb655849b1a833289e9ded18d
Dheerenda Singh <dsingh110096@gmail.com>
2020-08-02 15:33:52 +0530

    mongoose dublicate key,bad objectId,validation error etc added for error handling

--- middleware/error.js
+++ middleware/error.js
@@ -1,10 +1,38 @@
 /* (Important)==> To use any middleware we have to mount it to server first then only it will work otherwise not. */
 
-const errorHandler = (err, req, res, next) => {
-  //Log to console for developer
-  console.log(err.stack.bgWhite.red);
+//Error Response class file
+const ErrorResponse = require('../utils/errorResponse');
 
-  res.status(500).json({ success: false, error: err.message });
+const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
+  error.message = err.message;
+
+  //Log to console for developer
+  console.log(err.stack);
+
+  //Mongoose bad objectId
+  if (err.name === 'CastError') {
+    const message = `Resource not found with id of ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
+
+  //Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = `Resouce with ${Object.keys(err.keyValue)}: ${Object.values(
+      err.keyValue
+    )}, already exists.`;
+    error = new ErrorResponse(message, 400);
+  }
+
+  //Mongoose validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+
+  res
+    .status(error.statusCode || 500)
+    .json({ success: false, error: error.message || 'Server Error' });
 };
 
 module.exports = errorHandler;
