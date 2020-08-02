e8d8aa1d09335dbb8e1c85f099244d98f1efdda0
Dheerenda Singh <dsingh110096@gmail.com>
2020-08-02 17:19:33 +0530

    method added for login user

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,7 +12,7 @@ const gravatar = require('gravatar');
 
 //@desc     Register user
 //route     POST /api/v1/auth/register
-//access    Private
+//access    Public
 exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, password } = req.body;
 
@@ -30,6 +30,32 @@ exports.register = asyncHandler(async (req, res, next) => {
     avatar,
     password,
   });
+
+  sendTokenResponse(user, 201, res);
+});
+
+//@desc     Login user
+//route     GET /api/v1/auth/login
+//access    Private
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //Validate email and password
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide email and password', 400));
+  }
+
+  //check user exists in DB or not
+  const user = await User.findOne({ email }).select('+password');
+  if (!user) {
+    return next(new ErrorResponse('Invalid Credentials', 401));
+  }
+
+  //Check if password matches
+  const isMatch = await user.matchPassword(password);
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid Credentials', 401));
+  }
 
   sendTokenResponse(user, 201, res);
 });
