c465304b1987737406e0e9466baf87bd1855e55a
Dheerenda Singh <dsingh110096@gmail.com>
2020-08-02 17:01:30 +0530

    route added to get loged in user with protect middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
 
+//middleare file
+const { protect } = require('../middleware/auth');
+
 //Controller files
 //auth controller
-const { register } = require('../controllers/auth');
+const { register, getMe } = require('../controllers/auth');
 
 router.route('/register').post(register);
+router.route('/getme').get(protect, getMe);
 
 module.exports = router;
