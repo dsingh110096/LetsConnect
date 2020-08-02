64a637d41654a9b4ed4240455af7e6de3bc41635
Dheerenda Singh <dsingh110096@gmail.com>
2020-08-02 15:50:10 +0530

    async handler implemented

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,16 @@
 //Custum error response file
 const ErrorResponse = require('../utils/errorResponse');
 
+//AsyncHandler for removing trycatch block here means DRY(do not repeat your self)
+const asyncHandler = require('../middleware/async');
+
 //Model File
 const User = require('../models/User');
 
 //@desc     Register user
 //route     POST /api/v1/auth/register
 //access    Private
-exports.register = async (req, res, next) => {
-  try {
-    const user = await User.create(req.body);
-
-    res.status(201).json({ success: true, data: user });
-  } catch (err) {
-    next(err);
-  }
-};
+exports.register = asyncHandler(async (req, res, next) => {
+  const user = await User.create(req.body);
+  res.status(201).json({ success: true, data: user });
+});
