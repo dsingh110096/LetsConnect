87410126f1381a5e1565734f412e0f7520f8bec8
Dheerenda Singh <dsingh110096@gmail.com>
2020-08-02 14:50:58 +0530

    Register User functionality added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,16 @@
-//@desc     Get all auth users
-//route     GET /api/v1/auth
+//Model File
+const User = require('../models/User');
+
+//@desc     Register user
+//route     POST /api/v1/auth/register
 //access    Private
-exports.authUser = async (req, res) => {
-  res.send('Auth route');
+exports.register = async (req, res, next) => {
+  try {
+    const user = await User.create(req.body);
+
+    res.status(201).json({ success: true, data: user });
+  } catch (err) {
+    // res.status(400).json({ success: false, data: {} });
+    next(err);
+  }
 };
