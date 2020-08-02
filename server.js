a9cdc690a1f56644921cdb94c120fa0ed8814dba
Dheerenda Singh <dsingh110096@gmail.com>
2020-08-02 12:14:26 +0530

    Initial Setup for express server

---
+++ server.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+
+//custom middleware file
+
+//Loading env variables
+dotenv.config({ path: './config/config.env' });
+
+//Connection to mongoDB
+
+//Route files
+
+const app = express();
+
+//Body Parser to access (req.body)
+app.use(express.json());
+
+//Cookie parser
+
+//Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.get('/', (req, res) => {
+  res.send('API Running...');
+});
+
+//File upload
+
+//Mongo Sanitize Data
+
+//Adding Security headers
+
+//Prevent xss attacts
+
+//Prevent http param polution
+
+//Set static folder
+
+//Mount Routes
+
+//accessing env variable using process.env
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () => {
+  console.clear();
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+      .brightWhite.bold.underline
+  );
+});
+
+//handling unhandled promise rejection for mongoDB
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err}`.bgWhite.brightRed.bold.underline);
+  //close server & exit process
+  server.close(() => process.exit(1));
+});
