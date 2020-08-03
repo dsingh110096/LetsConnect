5d0bd15b120ad07d704dbcdcd4ca99682eeff143
Dheerenda Singh <dsingh110096@gmail.com>
2020-08-03 16:52:11 +0530

    routed added for create user profile and update user profile

--- routes/profile.js
+++ routes/profile.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
 
+const { protect } = require('../middleware/auth');
+
 //Controller files
 //profile controller
-const { getProfiles } = require('../controllers/profile');
+const {
+  getUserProfile,
+  createUserProfile,
+  updateUserProfile,
+} = require('../controllers/profile');
 
-router.route('/').get(getProfiles);
+router.route('/').post(protect, createUserProfile);
+router.route('/:id').put(protect, updateUserProfile);
+router.route('/me').get(protect, getUserProfile);
 
 module.exports = router;
