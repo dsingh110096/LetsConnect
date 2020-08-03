d8127887ff3e3eef0e46fc42182e0b58ef553090
Dheerenda Singh <dsingh110096@gmail.com>
2020-08-03 13:07:48 +0530

    model for user profile added

---
+++ models/Profile.js
@@ -0,0 +1,113 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+  },
+  company: {
+    type: String,
+  },
+  website: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  status: {
+    type: String,
+    required: [true, 'Please select a profession'],
+  },
+  skills: {
+    type: [String],
+    required: [true, 'Please provide skills you have'],
+  },
+  bio: {
+    type: String,
+  },
+  githubusername: {
+    type: String,
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        required: [true, 'Please provide a title'],
+      },
+      company: {
+        type: String,
+        required: [true, 'Please provide company name'],
+      },
+      location: {
+        type: String,
+      },
+      from: {
+        type: Date,
+        required: [true, 'Please provide date of joining'],
+      },
+      to: {
+        type: Date,
+      },
+      current: {
+        type: Boolean,
+        default: false,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: [true, 'Please provide school info'],
+      },
+      degree: {
+        type: String,
+        required: [true, 'Please provide degree info'],
+      },
+      fieldofstudy: {
+        type: String,
+        required: [true, 'Please provide field of study'],
+      },
+      from: {
+        type: Date,
+        required: [true, 'Please provide date of admission'],
+      },
+      to: {
+        type: Date,
+      },
+      current: {
+        type: Boolean,
+        default: false,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  social: {
+    youtube: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    linkedin: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model('Profile', ProfileSchema);
