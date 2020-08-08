d67236b5afc6f3e4e4c9a9189c86cb44cd8ecb77
Dheerenda Singh <dsingh110096@gmail.com>
2020-08-08 14:14:26 +0530

    model for post

---
+++ models/Post.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.objectId,
+    ref: 'User',
+  },
+  text: {
+    type: String,
+    required: [true, 'Please provide the text for post'],
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    {
+      user: {
+        type: mongoose.Schema.objectId,
+        ref: 'User',
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.objectId,
+        ref: 'User',
+      },
+      text: {
+        type: String,
+        required: [true, 'Please provide the text for comment'],
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now(),
+      },
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model('Post', PostSchema);
