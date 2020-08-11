6d00d0d9985b5b319dc914e8acdad42d49909596
Dheerenda Singh <dsingh110096@gmail.com>
2020-08-11 15:53:49 +0530

    profile reducer and action created

---
+++ client/src/actions/profile.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { setAlert } from './alert';
+import { GET_PROFILE, PROFILE_ERROR } from '../actions/types';
+
+//Get Current logged in user profile
+export const getCurrentLoggedInUserProfile = () => async (dispatch) => {
+  try {
+    const res = await axios.get('/api/v1/profile/me');
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
