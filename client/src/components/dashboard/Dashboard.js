e3ce6fbb4b70de7f9499c2e62047f45184a7558b
Dheerenda Singh <dsingh110096@gmail.com>
2020-08-11 15:55:03 +0530

    redux store connectd and mapStateToProps added

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getCurrentLoggedInUserProfile } from '../../actions/profile';
 import PropTypes from 'prop-types';
 
-const Dashboard = (props) => {
+const Dashboard = ({ getCurrentLoggedInUserProfile, auth, profile }) => {
+  useEffect(() => {
+    getCurrentLoggedInUserProfile();
+  }, [getCurrentLoggedInUserProfile]);
+
   return (
     <div>
       <h1>Dashboard</h1>
@@ -9,6 +15,17 @@ const Dashboard = (props) => {
   );
 };
 
-Dashboard.propTypes = {};
+Dashboard.propTypes = {
+  getCurrentLoggedInUserProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
+};
 
-export default Dashboard;
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  profile: state.profile,
+});
+
+export default connect(mapStateToProps, { getCurrentLoggedInUserProfile })(
+  Dashboard
+);
