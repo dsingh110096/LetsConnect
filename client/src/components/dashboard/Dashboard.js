77d55dd29651561089a892284d984f286f071794
Dheerenda Singh <dsingh110096@gmail.com>
2020-08-11 17:31:20 +0530

    spinner added for on profile load, profie has or has not content added

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,17 +1,39 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getCurrentLoggedInUserProfile } from '../../actions/profile';
 import PropTypes from 'prop-types';
+import Spinner from '../layout/Spinner';
 
-const Dashboard = ({ getCurrentLoggedInUserProfile, auth, profile }) => {
+const Dashboard = ({
+  getCurrentLoggedInUserProfile,
+  auth: { user },
+  profile: { profile, loading },
+}) => {
   useEffect(() => {
     getCurrentLoggedInUserProfile();
-  }, [getCurrentLoggedInUserProfile]);
+    //eslint-disable-next-line
+  }, []);
 
-  return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className='large text-primary'>Dashboard</h1>
+      <p className='lead'>
+        <i className='fas fa-user' /> Welcome {user && user.data.name}
+      </p>
+      {profile !== null ? (
+        <Fragment>Has</Fragment>
+      ) : (
+        <Fragment>
+          <p>You have not yet setup your profile, please add info</p>
+          <Link to='create-profile' className='btn btn-primary my-1'>
+            Create Profile
+          </Link>
+        </Fragment>
+      )}
+    </Fragment>
   );
 };
 
