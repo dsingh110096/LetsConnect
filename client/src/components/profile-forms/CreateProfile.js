59b975d43285243a1f125553b55627e83f9b2db9
Dheerenda Singh <dsingh110096@gmail.com>
2020-08-12 15:39:55 +0530

    action createOrUpdateUserProfile added, redirect to /dashboard using history object with rapping commponent into withRouter

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,8 +1,10 @@
 import React, { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createOrUpdateUserProfile } from '../../actions/profile';
 
-const CreateProfile = (props) => {
+const CreateProfile = ({ createOrUpdateUserProfile, history }) => {
   const initialState = {
     company: '',
     website: '',
@@ -39,6 +41,11 @@ const CreateProfile = (props) => {
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    createOrUpdateUserProfile(formData, history);
+  };
+
   return (
     <Fragment>
       <h1 className='large text-primary'>Create Your Profile</h1>
@@ -47,7 +54,7 @@ const CreateProfile = (props) => {
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className='form'>
+      <form className='form' onSubmit={(e) => onSubmit(e)}>
         <div className='form-group'>
           <select name='status' value={status} onChange={(e) => onChange(e)}>
             <option value='0'>* Select Professional Status</option>
@@ -214,6 +221,10 @@ const CreateProfile = (props) => {
   );
 };
 
-CreateProfile.propTypes = {};
+CreateProfile.propTypes = {
+  createOrUpdateUserProfile: PropTypes.func.isRequired,
+};
 
-export default CreateProfile;
+export default connect(null, { createOrUpdateUserProfile })(
+  withRouter(CreateProfile)
+);
