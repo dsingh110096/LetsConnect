54880f4e4f4e355a2019822dcac57d43a58a9523
Dheerenda Singh <dsingh110096@gmail.com>
2020-08-12 15:38:04 +0530

    Create or update user profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -17,3 +17,42 @@ export const getCurrentLoggedInUserProfile = () => async (dispatch) => {
     });
   }
 };
+
+//Create or update user profile
+export const createOrUpdateUserProfile = (
+  formData,
+  history,
+  edit = false
+) => async (dispatch) => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const res = await axios.post('/api/v1/profile', formData, config);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+
+    dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'));
+
+    if (!edit) {
+      history.push('/dashboard');
+    }
+  } catch (err) {
+    const errors = err.response.data.error;
+
+    if (errors) {
+      dispatch(setAlert(errors, 'danger'));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.data.error, status: err.response.status },
+    });
+  }
+};
