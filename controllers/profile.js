d69a546fc6fb380d46a99d9c454040ab696e6388
Dheerenda Singh <dsingh110096@gmail.com>
2020-08-12 18:31:31 +0530

    logic changed for creating and updating profile

--- controllers/profile.js
+++ controllers/profile.js
@@ -58,46 +58,30 @@ exports.getUserProfile = asyncHandler(async (req, res, next) => {
 //route     POST /api/v1/profile
 //access    Private
 exports.createAndUpdateUserProfile = asyncHandler(async (req, res, next) => {
-  //Destructuring variables from req.body
-  const {
-    company,
-    website,
-    location,
-    status,
-    skills,
-    bio,
-    githubusername,
-    youtube,
-    twitter,
-    facebook,
-    linkedin,
-    instagram,
-  } = req.body;
-
   //Build profile object
   const profileFields = {};
   profileFields.user = req.user.id;
-  if (company) profileFields.company = company;
-  if (website) profileFields.website = website;
-  if (location) profileFields.location = location;
-  if (status) profileFields.status = status;
-  if (skills) {
-    profileFields.skills = skills.split(',').map((skill) => skill.trim());
+
+  for (const key in req.body) {
+    if (key in req.body) {
+      if (key === 'skills') {
+        profileFields[key] = req.body.skills
+          .split(',')
+          .map((skill) => skill.trim());
+      } else {
+        profileFields[key] = req.body[key];
+      }
+    }
   }
-  if (bio) profileFields.bio = bio;
-  if (githubusername) profileFields.githubusername = githubusername;
 
-  //Build social object
+  // //Build social object
   profileFields.social = {};
-  if (youtube) profileFields.social.youtube = youtube;
-  if (twitter) profileFields.social.twitter = twitter;
-  if (instagram) profileFields.social.instagram = instagram;
-  if (facebook) profileFields.social.facebook = facebook;
-  if (linkedin) profileFields.social.linkedin = linkedin;
-
-  let profile = await Profile.findOne({ user: req.user.id });
+  for (const key in req.body) {
+    if (key in req.body) profileFields.social[key] = req.body[key];
+  }
 
   //check if profile exists then update the profile
+  let profile = await Profile.findOne({ user: req.user.id });
   if (profile) {
     profile = await Profile.findOneAndUpdate(
       { user: req.user.id },
@@ -107,7 +91,6 @@ exports.createAndUpdateUserProfile = asyncHandler(async (req, res, next) => {
         runValidators: true,
       }
     );
-    console.log(profile.id);
   }
 
   //if profile not exists then create it
