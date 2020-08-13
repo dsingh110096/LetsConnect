98e253e9a3a14893ff64aec67a9845ab7f4088e7
Dheerenda Singh <dsingh110096@gmail.com>
2020-08-13 09:34:06 +0530

    methods added for add education and add experience

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_PROFILE, PROFILE_ERROR } from '../actions/types';
+import {
+  GET_PROFILE,
+  PROFILE_ERROR,
+  ADD_USER_PROFILE_EXPERIENCE_EDUCATION,
+} from '../actions/types';
 
-//Get Current logged in user profile
+//Get current logged in user profile
 export const getCurrentLoggedInUserProfile = () => async (dispatch) => {
   try {
     const res = await axios.get('/api/v1/profile/me');
@@ -45,10 +49,83 @@ export const createOrUpdateUserProfile = (
     }
   } catch (err) {
     const errors = err.response.data.error;
+    if (errors) dispatch(setAlert(errors, 'danger'));
 
-    if (errors) {
-      dispatch(setAlert(errors, 'danger'));
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.data.error, status: err.response.status },
+    });
+  }
+};
+
+//Add User Profile Experience
+export const addUserProfileExperience = (
+  formData,
+  history,
+  edit = false
+) => async (dispatch) => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const res = await axios.put('/api/v1/profile/experience', formData, config);
+
+    dispatch({
+      type: ADD_USER_PROFILE_EXPERIENCE_EDUCATION,
+      payload: res.data,
+    });
+
+    dispatch(
+      setAlert(edit ? 'New Experience Added ' : 'Experience Updated', 'success')
+    );
+
+    if (edit || !edit) {
+      history.push('/dashboard');
     }
+  } catch (err) {
+    const errors = err.response.data.error;
+    if (errors) dispatch(setAlert(errors, 'danger'));
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.data.error, status: err.response.status },
+    });
+  }
+};
+
+//Add User Profile Education
+export const addUserProfileEducation = (
+  formData,
+  history,
+  edit = false
+) => async (dispatch) => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const res = await axios.put('/api/v1/profile/education', formData, config);
+
+    dispatch({
+      type: ADD_USER_PROFILE_EXPERIENCE_EDUCATION,
+      payload: res.data,
+    });
+
+    dispatch(
+      setAlert(edit ? 'New Education Added' : 'Education Updated', 'success')
+    );
+
+    if (edit || !edit) {
+      history.push('/dashboard');
+    }
+  } catch (err) {
+    const errors = err.response.data.error;
+    if (errors) dispatch(setAlert(errors, 'danger'));
 
     dispatch({
       type: PROFILE_ERROR,
