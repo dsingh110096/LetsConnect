1ffbc15be49264c22d9e41d64f3d67f3a39e7a34
Dheerenda Singh <dsingh110096@gmail.com>
2020-08-16 23:10:28 +0530

    postitem component added

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,13 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import PostItem from './PostItem.js';
 import { getAllPost } from '../../actions/post';
 
 const Posts = ({ post: { posts, loading }, getAllPost }) => {
   useEffect(() => {
     getAllPost();
   }, [getAllPost]);
-  return <div></div>;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className='large text-primary'>Posts</h1>
+      <p className='lead'>
+        <i className='fas fa-user' /> Welcome to the community
+      </p>
+      {/* Post Form Area */}
+      <div className='posts'>
+        {posts.count > 0 ? (
+          posts.data.map((post) => <PostItem key={post._id} post={post} />)
+        ) : (
+          <h4>No Posts Found...</h4>
+        )}
+      </div>
+    </Fragment>
+  );
 };
 
 Posts.propTypes = {
