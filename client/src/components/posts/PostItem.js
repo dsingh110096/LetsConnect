6eab0bc1ecb40d64e01aae89a67e779e78c63232
Dheerenda Singh <dsingh110096@gmail.com>
2020-08-17 09:52:18 +0530

    add and remove method added

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,7 +3,10 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Moment from 'react-moment';
+import { addLike, removeLike } from '../../actions/post';
 const PostItem = ({
+  addLike,
+  removeLike,
   auth,
   post: { _id, text, name, avatar, user, likes, comments, createdAt },
 }) => {
@@ -20,15 +23,23 @@ const PostItem = ({
         <p className='post-date'>
           Posted On: <Moment format='YYYY/MM/DD'>{createdAt}</Moment>
         </p>
-        <button type='button' className='btn btn-light'>
+        <button
+          type='button'
+          onClick={(e) => addLike(_id)}
+          className='btn btn-light'
+        >
           <i className='fas fa-thumbs-up'></i>
-          <span>{likes.length}</span>
+          <span> {likes.length}</span>
         </button>
-        <button type='button' className='btn btn-light'>
+        <button
+          type='button'
+          onClick={(e) => removeLike(_id)}
+          className='btn btn-light'
+        >
           <i className='fas fa-thumbs-down'></i>
         </button>
         <Link to={`/post/${_id}`} className='btn btn-primary'>
-          Discussion
+          Comments{' '}
           {comments.length > 0 && (
             <span className='comment-count'>{comments.length}</span>
           )}
@@ -46,10 +57,12 @@ const PostItem = ({
 PostItem.propTypes = {
   auth: PropTypes.object.isRequired,
   post: PropTypes.object.isRequired,
+  addLike: PropTypes.func.isRequired,
+  removeLike: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   auth: state.auth,
 });
 
-export default connect(mapStateToProps, {})(PostItem);
+export default connect(mapStateToProps, { addLike, removeLike })(PostItem);
