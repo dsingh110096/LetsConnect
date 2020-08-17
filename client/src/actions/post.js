e588de7be36a6a55b9f4025cb8855c8024956e46
Dheerenda Singh <dsingh110096@gmail.com>
2020-08-17 09:51:22 +0530

    method for update and remove likes

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,4 +1,4 @@
-import { GET_POSTS, POST_ERROR } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
 import axios from 'axios';
 
 //Get All Post
@@ -10,6 +10,39 @@ export const getAllPost = () => async (dispatch) => {
     dispatch({
       type: POST_ERROR,
       paylod: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+//Add like to post
+export const addLike = (postId) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/v1/posts/likes/${postId}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { postId, likes: res.data.data },
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      paylod: { msg: 'Unable to Like', status: 400 },
+    });
+  }
+};
+
+//Remove like from post
+export const removeLike = (postId) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/v1/posts/removelike/${postId}`);
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { postId, likes: res.data.data },
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      paylod: { msg: 'Unable to remove like', status: 400 },
     });
   }
 };
