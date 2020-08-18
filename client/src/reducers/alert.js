83464541815b481248c57cf7f2ad60dd5f3f8992
Dheerenda Singh <dsingh110096@gmail.com>
2020-08-18 13:47:51 +0530

    iteration of alerts stoped

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -8,7 +8,7 @@ export default function (state = initialState, action) {
 
   switch (type) {
     case SET_ALERT:
-      return [...state, payload];
+      return [payload];
     case REMOVE_ALERT:
       return state.filter((alert) => alert.id !== payload);
     default:
