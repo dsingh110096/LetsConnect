1a8b1f40ae8734c68ba87f69313945b317b031f5
Dheerenda Singh <dsingh110096@gmail.com>
2020-09-01 11:02:19 +0530

    fix

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -2,10 +2,10 @@ const { override } = require('customize-cra');
 const cspHtmlWebpackPlugin = require('csp-html-webpack-plugin');
 
 const cspConfigPolicy = {
-  'default-src': "'none'",
+  'default-src': "'self'",
   'base-uri': "'self'",
   'object-src': "'none'",
-  'script-src': ["'self'"],
+  'script-src': ["'self'", "'unsafe-inline'"],
   'style-src': ["'self'"],
 };
 
