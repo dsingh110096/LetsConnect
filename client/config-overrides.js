94cb507cbb69e105104c8f23119c68acca2a6058
Dheerenda Singh <dsingh110096@gmail.com>
2020-09-01 10:30:21 +0530

    fix

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -2,11 +2,6 @@ const { override } = require('customize-cra');
 const cspHtmlWebpackPlugin = require('csp-html-webpack-plugin');
 
 const cspConfigPolicy = {
-  'default-src': "'none'",
-  'base-uri': "'self'",
-  'object-src': "'none'",
-  'script-src': ["'self'"],
-  'style-src': ["'self'"],
   'img-src': ["'self'", 'https://gravatar.com'],
 };
 
